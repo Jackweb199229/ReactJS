64ab3369750261532ff73c52240824d0eec20629
Jackweb199229 <jackweb199229@gmail.com>
2026-01-11 00:38:42 +0530

    array of object added

--- day-01/secondapp/src/App.js
+++ day-01/secondapp/src/App.js
@@ -1,16 +1,81 @@
+import "./App.css";
+import "./index.css";
+import Card from "./Component/Card";
 
-import './App.css';
-import './index.css';
-import Card from './Component/Card';
+const jobOpenings = [
+  {
+    brandLogo:
+      " https://img.freepik.com/premium-vector/google-logo_1273375-1572.jpg?semt=ais_hybrid&w=740&q=80 ",
+    companyName: "Google",
+    datePosted: "5 days ago",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$45/hour",
+    location: "Mumbai, India",
+  },
+  {
+    brandLogo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSURsmbRJ4V2i53XX70PU8bCeZMXWjVXgMvh-TQc68pMX6VRJ6VDVLSlmo&s",
+    companyName: "Meta",
+    datePosted: "2 days ago",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$65/hour",
+    location: "Bangalore, India",
+  },
+  {
+    brandLogo:
+      " https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg",
+    companyName: "Amazon",
+    datePosted: "10 days ago",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$35/hour",
+    location: "Hyderabad, India",
+  },
+  {
+    brandLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+    companyName: "Apple",
+    datePosted: "7 days ago",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$55/hour",
+    location: "Mumbai, India",
+  },
+  {
+    brandLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+    companyName: "Netflix",
+    datePosted: "1 day ago",
+    tag1: "Contract",
+    tag2: "Senior Level",
+    pay: "$70/hour",
+    location: "Remote",
+  },
+  {
+    brandLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    companyName: "Microsoft",
+    datePosted: "4 days ago",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$48/hour",
+    location: "Noida, India",
+  },
+];
 
 function App() {
   return (
     <div className="Parent">
-      <Card />
-      <Card />
-      <Card />
+      {jobOpenings.map((job, index) => (
+        console.log(index),
+        <Card brandLogo={job.brandLogo} companyName={job.companyName} datePosted={job.datePosted} 
+        tag1={job.tag1} tag2={job.tag2} pay={job.pay} location={job.location} />
+      ))}
+
     </div>
   );
 }
- 
+
 export default App;
--- day-01/secondapp/src/Component/Card.js
+++ day-01/secondapp/src/Component/Card.js
@@ -1,18 +1,14 @@
 import "./Card.css";
 
-function Card() {
+function Card(props) {
   return (
     <div className="Card">
       <div>
         <div className="top">
-          <img
-            src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTZe2_cwIfo0yLvg5c9kvfNH7QhekWrjSuSA&s "
-            alt=""
-          />
+          <img src={props.brandLogo} alt="company logo" />
 
           <button>
-            {" "}
-            Save{" "}
+            Save
             <span className="icon material-symbols-outlined">
               bookmark
             </span>{" "}
@@ -21,21 +17,21 @@ function Card() {
 
         <div className="center">
           <h3>
-            Amazon <span>5 days ago </span>
+            {props.companyName} <span>{props.datePosted} </span>
           </h3>
           <h2>Senior UI/UX Designer </h2>
 
           <div className="tag">
-            <h4> Part Time </h4>
-            <h4>Senior Level </h4>
+            <h4> {props.tag1} </h4>
+            <h4>{props.tag2} </h4>
           </div>
         </div>
       </div>
 
       <div className="bottom">
         <div>
-          <h3>$120/hr </h3>
-          <p> Mumbai, India </p>
+          <h3>{props.pay} </h3>
+          <p> {props.location} </p>
         </div>
         <button> Apply Now </button>
       </div>
